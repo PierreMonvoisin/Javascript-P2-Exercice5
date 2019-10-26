8382a24e9f5a69ee941c9326b57a2340289b5b67
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-10-26 15:04:30 +0200

    Exercice 5 terminé avec une seule fonction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,9 @@
-let image1 = document.getElementById("image1");
-let image2 = document.getElementById("image2");
-let image3 = document.getElementById("image3");
-let image4 = document.getElementById("image4");
-let image5 = document.getElementById("image5");
+let swap = function() {
+  this.src = "./assets/img/" + this.id + "_2.jpg";
+}
 
-// FIRST IMAGE
-image1.onmouseenter = function() {
-  image1.src = "./assets/img/image1_2.jpg";
-};
-image1.onmouseout = function() {
-  image1.src = "./assets/img/image1.jpg";
-};
-
-// SECOND IMAGE
-image2.onmouseenter = function() {
-  image2.src = "./assets/img/image2_2.jpg";
-};
-image2.onmouseout = function() {
-  image2.src = "./assets/img/image2.jpg";
-};
-
-// THIRD IMAGE
-image3.onmouseenter = function() {
-  image3.src = "./assets/img/image3_2.jpg";
-};
-image3.onmouseout = function() {
-  image3.src = "./assets/img/image3.jpg";
-};
-
-// FOURTH IMAGE
-image4.onmouseenter = function() {
-  image4.src = "./assets/img/image4_2.jpg";
-};
-image4.onmouseout = function() {
-  image4.src = "./assets/img/image4.jpg";
-};
-
-// FIFTH IMAGE
-image5.onmouseenter = function() {
-  image5.src = "./assets/img/image5_2.jpg";
-};
-image5.onmouseout = function() {
-  image5.src = "./assets/img/image5.jpg";
-};
+document.getElementById('image1').onmouseenter = swap ;
+document.getElementById('image2').onmouseenter = swap ;
+document.getElementById('image3').onmouseenter = swap ;
+document.getElementById('image4').onmouseenter = swap ;
+document.getElementById('image5').onmouseenter = swap ;
