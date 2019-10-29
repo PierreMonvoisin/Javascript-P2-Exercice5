98fc5e760f88d62d75a5df632a0bec87e08d42ac
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-10-29 11:42:26 +0100

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 let swap = function() {
-  this.src = "./assets/img/" + this.id + "_2.jpg";
+  //Changement de la source de l'image en fonction de l'ID de l'image
+  this.src = './assets/img/' + this.id + '_2.jpg';
+  // this.class = "nouvelleClasse"; NE FONCTIONNE PAS
 }
 
 document.getElementById('image1').onmouseenter = swap ;
